d4bf12a2b665083ec53996da6d831d8d523ca178
Golonac94 <rparedest@uni.pe>
2023-12-17 08:46:51 -0500

    Initial Commit

---
+++ ProyectoPruebaSwiftUI/ProyectoPruebaSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProyectoPruebaSwiftUIApp.swift
+//  ProyectoPruebaSwiftUI
+//
+//  Created by Romario Paredes on 17/12/23.
+//
+
+import SwiftUI
+
+@main
+struct ProyectoPruebaSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
